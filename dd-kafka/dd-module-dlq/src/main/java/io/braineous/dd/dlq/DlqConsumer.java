36322eaf3a0b8307defca9d7e93dcd5264af85fb
Sohil Shah - BabyBoy <braineous.engineering@gmail.com>
2026-01-03 12:36:20 -0600

    feat -> dd_module_dlq -> DLQ

--- dd-kafka/dd-module-dlq/src/main/java/io/braineous/dd/dlq/DlqConsumer.java
+++ dd-kafka/dd-module-dlq/src/main/java/io/braineous/dd/dlq/DlqConsumer.java
@@ -8,7 +8,8 @@ import org.eclipse.microprofile.reactive.messaging.Incoming;
 public class DlqConsumer {
 
  @Incoming("dead_letter.system.in")
- public void handleSystemFailure(String payload) { }
+ public void handleSystemFailure(String payload){
+ }
 
  @Incoming("dead_letter.domain.in")
  public void handleDomainFailure(String payload) { }
